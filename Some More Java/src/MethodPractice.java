4e1e741ee96e9067c8103db64929cc9e503abe5c
mhs <mhs@MHS-Science3-16>
2017-10-25 17:09:22 -0700

    hi

---
+++ Some More Java/src/MethodPractice.java
@@ -0,0 +1,20 @@
+
+public class MethodPractice {
+public static void main(String []args){ 
+}
+public double max(double a,double b){
+	if(a<b){
+		return(b);
+}else{
+	return(a); 
+	}
+}
+public double exponent(double c,int d){
+	for(c*c)
+	
+	
+
+
+
+}
+}}
